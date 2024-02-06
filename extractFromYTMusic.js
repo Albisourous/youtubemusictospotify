9c75e0024f10e8c260d17419e8c06e29531f0b27
Albin Shrestha <shrestha_albin@yahoo.com>
2024-02-05 19:12:41 -0800

    Updated to download the file when script is run
    Updated first output with 100 first songs on my playlist

--- extractFromYTMusic.js
+++ extractFromYTMusic.js
@@ -1,6 +1,8 @@
 var contentsElement = document.querySelector('#contents.style-scope.ytmusic-playlist-shelf-renderer');
 var songs = document.querySelectorAll('ytmusic-responsive-list-item-renderer.style-scope.ytmusic-playlist-shelf-renderer');
 
+var songList = ""; // Final song list
+
 songs.forEach(song => {
     const artist = song.querySelector('ytmusic-responsive-list-item-renderer.style-scope.ytmusic-playlist-shelf-renderer yt-formatted-string.flex-column.style-scope[title]');
     const title = song.querySelector('yt-formatted-string.title.style-scope');
@@ -30,6 +32,17 @@ songs.forEach(song => {
         titleOut += titleText;
     }
 
-    console.log(artistOut + " - " + titleOut);
+    songList += artistOut + " - " + titleOut + "\n";
 });
+
+// Create a Blob with the text content
+var blob = new Blob([songList], { type: 'text/plain' });
+
+// Create a download link
+var downloadLink = document.createElement('a');
+downloadLink.href = URL.createObjectURL(blob);
+downloadLink.download = 'playlist_output.txt';
+
+// Trigger a click on the link to start the download
+downloadLink.click();
 

0d547d999bfef0298bf8175dd8016e24e83f0505
Albin Shrestha <shrestha_albin@yahoo.com>
2024-02-05 19:56:56 -0800

    .gitignore is now working

--- credentials.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['SPOTIPY_CLIENT_ID'] = 'your-spotify-client-id'
-os.environ['SPOTIPY_CLIENT_SECRET'] = 'your-spotify-client-secret'
-os.environ['SPOTIPY_REDIRECT_URI'] = 'your-app-redirect-url'

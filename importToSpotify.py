2cf00351a47c37894c8519c74c13c3aa9f7db827
Albin Shrestha <shrestha_albin@yahoo.com>
2024-02-06 17:47:27 -0800

    Script to import to spotify

--- importToSpotify.py
+++ importToSpotify.py
@@ -1,19 +1,38 @@
-# Inspired by https://github.com/smtchahal/spotify-import/blob/master/spotify_import.py
+# Lesson: ChatGPT is powerful for scripting
 
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from credentials import set_spotify_credentials
+import json
+from credentials import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIRECT_URI
 
-# Set Spotify credentials from credentials.py
-set_spotify_credentials()
+def read_song_list(file_path):
+    with open(file_path, 'r') as file:
+        songs = file.read().splitlines()
+    return songs
 
-# Access credentials from environment variables
-client_id = os.environ.get('SPOTIPY_CLIENT_ID')
-client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
-redirect_uri = os.environ.get('SPOTIPY_REDIRECT_URI')
+def add_songs_to_library(spotify, songs):
+    for song in songs:
+        results = spotify.search(q=song, type='track', limit=1)
+        if results['tracks']['items']:
+            track_id = results['tracks']['items'][0]['id']
+            spotify.current_user_saved_tracks_add([track_id])
+            print(f"Added '{song}' to your library.")
+        else:
+            print(f"Couldn't find '{song}' on Spotify.")
 
 def main():
+    file_path = 'playlist_output.txt'
+    songs = read_song_list(file_path)
 
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        redirect_uri=SPOTIPY_REDIRECT_URI,
+        scope='user-library-modify'
+    ))
 
-if __name__ == '__main__':
+    add_songs_to_library(sp, songs)
+
+if __name__ == "__main__":
     main()
